2853b040893a014069c082fb301decaebed12773
Avadhut C <42667455+avadhutc@users.noreply.github.com>
2019-03-20 16:40:41 +0530

    Update resnet_py.py

--- resnet_py.py
+++ resnet_py.py
@@ -257,4 +257,4 @@ class ResnetBuilder(object):
 
     @staticmethod
     def build_resnet_152(input_shape, num_outputs):
-return ResnetBuilder.build(input_shape, num_outputs, bottleneck, [3, 8, 36, 3])
+        return ResnetBuilder.build(input_shape, num_outputs, bottleneck, [3, 8, 36, 3])
